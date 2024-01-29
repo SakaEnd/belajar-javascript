9d392f1399cc1eac5d9130604f11929145c6b27f
SakaEnd <sakaajasih@gmail.com>
2024-01-29 13:03:26 +0700

    Fungsi

---
+++ fungai.js
@@ -0,0 +1,12 @@
+var x = "25";
+var y = "3.14";
+
+var toNum = parseInt(x);
+var toFloat = parseFloat(y);
+console.log(x + toNum);
+
+function jumlah(a, b) {
+    return a + b;
+}
+
+alert(jumlah(5, 20));
