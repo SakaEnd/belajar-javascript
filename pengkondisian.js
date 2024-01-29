7ff2a26384c1e97fb0a53a1c16f0af69026faebb
SakaEnd <sakaajasih@gmail.com>
2024-01-29 14:07:55 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("masukan nomor :");
+// alert("selama datang " + nama);
+
+if(x % 2 == 0) {
+    alert(x + "bilangan Genap");
+} else if(x % 2 == 1) {
+    alert(x + "bilangan Ganjil");
+} else {
+    alert("data bukan angka")
+}
